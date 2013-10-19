acdb01920971385aa55b7e5d49dd97d3dc9d2e5c
ralf <ralf@localhost>
2013-10-19 10:48:42 +0000

    Implemented correct handling of multi-month data.
    svn path=/trunk/; revision=5

--- weatherstation/pcwetterstation.py
+++ weatherstation/pcwetterstation.py
@@ -1,30 +1,33 @@
 """Provides functions for managing weather data compatible to the software PC-Wetterstation.
 
 Functions:
-write:                          Writes a weather data CSV-file compatible to PC-Wetterstation.
+write:                          Writes a weather data CSV-file compatible to PC-Wetterstation for arbitrary data.
+writesinglemonth:               writes a weather data CSV-file compatible to PC-Wetterstation for a single month.
 convertTo:                      Converts weather data into units and format compatible to PC-Wetterstation.
 """
 import csv
-import datetime
+from datetime import datetime as dt
 from collections import OrderedDict
 
 import constants
 import utilities
 
 
+
 def write( data_folder, rain_calib_factor, last_old_rain_counter, station_name, station_height, station_type, export_data, sensor_list ):
-    """Writes a CSV-file with the weather data compatible to PC-Wetterstation.
+    """Writes a CSV-file with the weather data compatible to PC-Wetterstation for arbitrary data.
     
     Args:
     data_folder:                Folder where the CSV-file for PC-Wetterstation will be stored. It must be given relative to the current path.
-                                The file name will be automatically "EXP_MM_YY.CSV" with MM and YY being the month an the year of the datasets.
+                                The file names will be automatically "EXP_MM_YY.CSV" with MM and YY being the months and the years of the datasets.
     rain_calib_factor:          Calibration factor of the rain sensor (1.000 if the rain sensor has the original area).
     last_old_rain_counter:      Last rain counter setting before the first dataset of this file (in mm).
     station_name:               ID of the station (typically three letters, for example ERL).
     station_height:             Altitude of the station (in meters).
     station_type:               Information string on the detailed type of the weather station (producer, ...).
-    export_data:                Data to be written on file. The required format is a list of ordered dicts 
-                                with the key being registered in sensor_list. It must contain at least the following information in this order:
+    export_data:                Data to be written on file. It is not required to be sorted regarding time.
+                                The required format is a list of ordered dicts with the key being registered in sensor_list. It must contain at least 
+                                the following information in this order:
                                     - date of the data in the format dd.mm.yyyy
                                     - time of the data (local time) in the format mm:hh
                                     - all measured data according to PC-Wetterstation specification with:
@@ -32,7 +35,8 @@ def write( data_folder, rain_calib_factor, last_old_rain_counter, station_name, 
                                             * rain in mm since last recording
                                             * temperatures in degree Celsius
                                             * pressure in hPa
-                                            * humidities in percent  
+                                            * humidities in percent
+                                
     sensor_list:                Ordered dict containing the mapping of all sensors to the index of the sensor in the weatherstation and the software PC-Wetterstation,
                                 the name and the units of the sensors. The keys must be identical to that used in the 'export_data'.
 
@@ -42,8 +46,62 @@ def write( data_folder, rain_calib_factor, last_old_rain_counter, station_name, 
     Raises:
     IOError:                    An error occurred accessing the file.
     """
+    getmonth = lambda x: dt.strptime( x['date'], '%d.%m.%Y' )
+    
+    # determine the months existing in the data
+    export_data = sorted( export_data, key = lambda k: dt.strptime( k['date'] + ' ' + k['time'], '%d.%m.%Y %H:%M') )
+    months_set = { ( getmonth(x).month, getmonth(x).year ) for x in export_data }
+    
+    # write the data separately for each month into a file
+    for curr_month in months_set:
+        monthly_export_data = [ x for x in export_data if ( getmonth(x).month == curr_month[0] and getmonth(x).year == curr_month[1] ) ]
+        writesinglemonth( data_folder, rain_calib_factor, last_old_rain_counter, station_name, station_height, station_type, monthly_export_data, sensor_list )
+
+
+def writesinglemonth( data_folder, rain_calib_factor, last_old_rain_counter, station_name, station_height, station_type, export_data, sensor_list ):
+    """Writes a CSV-file with the weather data compatible to PC-Wetterstation for a certain single month.
+    
+    Args:
+    data_folder:                Folder where the CSV-file for PC-Wetterstation will be stored. It must be given relative to the current path.
+                                The file name will be automatically "EXP_MM_YY.CSV" with MM and YY being the month and the year of the datasets.
+    rain_calib_factor:          Calibration factor of the rain sensor (1.000 if the rain sensor has the original area).
+    last_old_rain_counter:      Last rain counter setting before the first dataset of this file (in mm).
+    station_name:               ID of the station (typically three letters, for example ERL).
+    station_height:             Altitude of the station (in meters).
+    station_type:               Information string on the detailed type of the weather station (producer, ...).
+    export_data:                Data to be written on file. It must be only from one single month. It is not required to be sorted regarding time.
+                                The required format is a list of ordered dicts with the key being registered in sensor_list. It must contain at least 
+                                the following information in this order:
+                                    - date of the data in the format dd.mm.yyyy
+                                    - time of the data (local time) in the format mm:hh
+                                    - all measured data according to PC-Wetterstation specification with:
+                                            * wind speeds in km/h
+                                            * rain in mm since last recording
+                                            * temperatures in degree Celsius
+                                            * pressure in hPa
+                                            * humidities in percent
+                                
+    sensor_list:                Ordered dict containing the mapping of all sensors to the index of the sensor in the weatherstation and the software PC-Wetterstation,
+                                the name and the units of the sensors. The keys must be identical to that used in the 'export_data'.
+
+    Returns:
+    None
+
+    Raises:
+    IOError:                    An error occurred accessing the file.
+    AssertionError:             The data in 'export_data' is from more than one month.
+    """
+    # Sort data
+    export_data = sorted( export_data, key = lambda k: dt.strptime( k['date'] + ' ' + k['time'], '%d.%m.%Y %H:%M') )
+
+    # Check if data is really only from one month
+    getmonth = lambda x: dt.strptime( x['date'], '%d.%m.%Y' )
+    months_set = { ( getmonth(x).month, getmonth(x).year ) for x in export_data }
+    if len( months_set ) > 1:
+        raise AssertionError( 'The data is from more than one month.' )
+
     # Generate file name assuming that all datasets are from one month
-    firstDate = datetime.datetime.strptime( export_data[0]['date'], '%d.%m.%Y')
+    firstDate = dt.strptime( export_data[0]['date'], '%d.%m.%Y')
     data_file_name = data_folder + '/EXP' + firstDate.strftime('%m_%y') + '.csv'
 
     # Generate settings line for the CSV-file
@@ -82,7 +140,8 @@ def convertTo(read_data, last_old_rain_counter, sensor_list):
     
     Args:
     read_data:                  List containing the new read data to be processed. For each time a list containing the data with the 
-                                indices specified in 'sensor_list' must be present. The units of the data must be as follows:
+                                indices specified in 'sensor_list' must be present. It is not required to be sorted. 
+                                The units of the data must be as follows:
                                     * time of measurement as standard c-time in seconds since epoch (CET considering daylight saving)
                                     * wind speeds in m/s
                                     * rain as absolute rain counter since the last reset at the measurement time (in mm)
@@ -96,7 +155,8 @@ def convertTo(read_data, last_old_rain_counter, sensor_list):
     
     Returns:
     export_data:                List containing the weather data compatible to PC-Wetterstation. The format is a list of ordered dicts 
-                                with the key being registered in sensor_list. It contains at least the following information in this order:
+                                with the key being registered in sensor_list. It contains at least the following information in this order 
+                                and is sorted according to date and time:
                                     - date of the data in the format dd.mm.yyyy
                                     - time of the data (CET time considering daylight saving) in the format mm:hh
                                     - all measured data according to PC-Wetterstation specification with:
@@ -134,9 +194,12 @@ def convertTo(read_data, last_old_rain_counter, sensor_list):
 
     # Convert date stamps (date zone and DST according to system settings)
     for line in export_data[:]:
-        curr_time = datetime.datetime.fromtimestamp( int( line['date'] ) );
+        curr_time = dt.fromtimestamp( int( line['date'] ) );            # import ctime seconds since epoch
         line['date'] = curr_time.strftime( '%d.%m.%Y' )
         line['time'] = curr_time.strftime( '%H:%M' )
+
+    # Sort data
+    export_data = sorted( export_data, key = lambda k: dt.strptime( k['date'] + ' ' + k['time'], '%d.%m.%Y %H:%M') )
 
     # Perform required unit convertions
     for line in export_data[:]:
@@ -151,7 +214,6 @@ def convertTo(read_data, last_old_rain_counter, sensor_list):
     for export_line, amount in zip( export_data[:], rain_amounts ):
         export_line['rainCounter'] = str( amount );                      # set to rain amount differences since the last dataset before the current (in mm)
 
-    last_dataset = export_data.pop()
-    last_dataset_time = datetime.datetime.strptime( last_dataset['date'] + ' ' + last_dataset['time'], '%d.%m.%Y %H:%M') # the accuracy is minutes
+    last_dataset_time = dt.strptime( export_data[-1]['date'] + ' ' + export_data[-1]['time'], '%d.%m.%Y %H:%M') # the accuracy is minutes
     last_dataset_rain_counter = rain_counters.pop()
     return export_data, last_dataset_time, last_dataset_rain_counter
