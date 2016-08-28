371144c7fad9be2d0b9278d30cdf8a89bc68467a
ralf <ralf@localhost>
2016-08-28 19:17:16 +0000

    Improved the normalization of the SQL-database.
    svn path=/branches/database/; revision=64

--- weatherstation/weathernetwork/server/database.py
+++ weatherstation/weathernetwork/server/database.py
@@ -1,6 +1,7 @@
 import threading
 import sqlite3
 import datetime
+from weathernetwork.common.weatherdataset import WeatherDataset
 
 class WeatherDB(object):
     """
@@ -18,7 +19,7 @@ class WeatherDB(object):
         with self._sql:
             self._sql.execute( "CREATE TABLE IF NOT EXISTS WeatherStations \
                                     ( \
-                                    identifier TEXT PRIMARY KEY, \
+                                    identifier TEXT NOT NULL PRIMARY KEY, \
                                     device TEXT NOT NULL, \
                                     location TEXT NOT NULL, \
                                     latitude FLOAT NOT NULL, \
@@ -29,18 +30,30 @@ class WeatherDB(object):
                                     ( \
                                     stationID INTEGER NOT NULL, \
                                     time DATETIME NOT NULL, \
-                                    temperature FLOAT, \
-                                    humidity FLOAT, \
                                     rainGauge FLOAT, \
                                     pressure FLOAT, \
                                     UV FLOAT, \
-                                    windDirection FLOAT, \
-                                    windSpeed FLOAT, \
-                                    windGust FLOAT, \
-                                    windTemp FLOAT, \
-                                    FOREIGN KEY(stationID) REFERENCES WeatherStations(identifier) ON DELETE CASCADE, \
+                                    FOREIGN KEY(stationID) \
+                                        REFERENCES WeatherStations(identifier) \
+                                        ON DELETE CASCADE \
+                                        ON UPDATE CASCADE, \
                                     PRIMARY KEY(stationID, time) \
                                     )" )
+
+            self._sql.execute( "CREATE TABLE IF NOT EXISTS WindSensor \
+                                    ( \
+                                    stationID INTEGER NOT NULL, \
+                                    time DATETIME NOT NULL, \
+                                    direction FLOAT, \
+                                    speed FLOAT, \
+                                    gusts FLOAT, \
+                                    temperature FLOAT, \
+                                    FOREIGN KEY(stationID, time) \
+                                        REFERENCES WeatherData(stationID, time) \
+                                        ON DELETE CASCADE \
+                                        ON UPDATE CASCADE, \
+                                    PRIMARY KEY(stationID, time) \
+                                    )")
 
             self._sql.execute("PRAGMA foreign_keys = ON")
 
@@ -52,62 +65,35 @@ class WeatherDB(object):
         self._sql.close()
 
 
-    def add_dataset(self, station_identifier, dataset):
+    def add_dataset(self, station_identifier, data):
         """
         Adds a new dataset to the database.
-        """
-        self._insert_dataset(station_identifier, dataset, False)
-
-
-    def replace_dataset(self,station_identifier, dataset):
-        """
-        Replaces an existing dataset. If this dataset does not exist, it will be inserted.
-        """
-        self._insert_dataset(station_identifier, dataset, True)
-
-
-    def _insert_dataset(self, station_identifier, data, is_replace):
-        """
-        Inserts the dataset into the database (inserting or replacing as required)
-        Note: The performance is limited to about 50 row/s. Add multiple rows at once for better performance.
+        Note: The performance is limited to about 50 commits/s. Add multiple rows at once for better performance.
         """
         if not isinstance(data,list):
             data = [data]
 
         with self._lock:
             with self._sql:
-                if is_replace:
-                    commandStr = "REPLACE"
-                else:
-                    commandStr = "INSERT"
-
                 # add the dataset to the database
                 for dataset in data:
-                    # read the data for that dataset
                     time = dataset.get_time();
                     temperature = dataset.get_temperature();
                     humidity = dataset.get_humidity();
                     rainGauge = dataset.get_rain_gauge();
                     pressure = dataset.get_pressure();
                     UV = dataset.get_UV();
-                    windDirection, windSpeed, windGust, windTemp = dataset.get_wind();
 
-                    # write it to the database
                     try:
-                        self._sql.execute( "%s INTO WeatherData ( \
-                                                time, \
-                                                stationID, \
-                                                temperature, \
-                                                humidity, \
-                                                rainGauge, \
-                                                pressure, \
-                                                UV, \
-                                                windDirection, \
-                                                windSpeed, \
-                                                windGust, \
-                                                windTemp \
-                                                ) VALUES (?,(SELECT identifier from WeatherStations WHERE identifier=(?)),?,?,?,?,?,?,?,?,?)" % commandStr, \
-                                                    ( time.replace(microsecond=0), station_identifier, temperature, humidity, rainGauge, pressure, UV, windDirection, windSpeed, windGust, windTemp ) )
+                        self._sql.execute( " \
+                            INSERT INTO WeatherData ( \
+                                time, \
+                                stationID, \
+                                rainGauge, \
+                                pressure, \
+                                UV \
+                            ) VALUES (?,(SELECT identifier from WeatherStations WHERE identifier=(?)),?,?,?)", \
+                            ( self._round_time(time), station_identifier, rainGauge, pressure, UV ) )
                     except sqlite3.Error as e:
                         if "NULL" in e.args[0]:
                             raise NameError("The station does not exist in the database")
@@ -115,6 +101,63 @@ class WeatherDB(object):
                             raise NameError("For the given station, a dataset for the given time already exists in the database")
                         else:
                             raise NameError(e)
+
+                    # write the wind sensor information (here it is already guaranteed that a dataset for the given station and time existing in the database)
+                    wind_direction, wind_speed, wind_gust, wind_temp = dataset.get_wind();
+                    self._sql.execute(" \
+                        INSERT INTO WindSensor ( \
+                            time, \
+                            stationID, \
+                            direction, \
+                            speed, \
+                            gusts, \
+                            temperature \
+                        ) VALUES (?,?,?,?,?,?)", \
+                        ( self._round_time(time), station_identifier, wind_direction, wind_speed, wind_gust, wind_temp ) )       
+
+
+    def replace_dataset(self, station_identifier, data):
+        """
+        Replaces an existing dataset.
+        Note: The performance is limited to about 50 commits/s. Add multiple rows at once for better performance.
+        """
+        if not isinstance(data,list):
+            data = [data]
+
+        with self._lock:
+            with self._sql:
+                for dataset in data:
+                    time = dataset.get_time();
+                    temperature = dataset.get_temperature();
+                    humidity = dataset.get_humidity();
+                    rainGauge = dataset.get_rain_gauge();
+                    pressure = dataset.get_pressure();
+                    UV = dataset.get_UV();
+
+                    num_updated_rows = self._sql.execute( " \
+                        UPDATE WeatherData \
+                        SET rainGauge=(?), pressure=(?), UV=(?) \
+                        WHERE time=(?) AND stationID=(?)", \
+                        ( rainGauge, pressure, UV, self._round_time(time), station_identifier ) ).rowcount           
+           
+                    if num_updated_rows == 0:
+                        raise NameError("No entry exists for the requested station at the requested time")
+
+                    # write the wind sensor information (here it is already guaranteed that a dataset for the given station and time existing in the database)
+                    wind_direction, wind_speed, wind_gust, wind_temp = dataset.get_wind();
+
+                    self._sql.execute( " \
+                        UPDATE WindSensor \
+                        SET direction=(?), speed=(?), gusts=(?), temperature=(?) \
+                        WHERE time=(?) AND stationID=(?)", \
+                        ( wind_direction, wind_speed, wind_gust, wind_temp, self._round_time(time), station_identifier ) )           
+
+
+    def _round_time(self, time):
+        """
+        Rounds the given time to seconds
+        """
+        return time.replace(microsecond=0)
 
 
     def remove_dataset(self, station_ID, time):
@@ -128,10 +171,12 @@ class WeatherDB(object):
         with self._lock:
             with self._sql:
                 for curr_time in time:
-                    num_deleted_rows += self._sql.execute( "DELETE \
-                                                            FROM WeatherData \
-                                                            WHERE stationID=(?) AND time=(?)", \
-                                                                ( station_ID, curr_time.replace(microsecond=0) ) ).rowcount
+                    # all cascaded datasets are also automatically deleted
+                    num_deleted_rows += self._sql.execute( " \
+                        DELETE \
+                        FROM WeatherData \
+                        WHERE stationID=(?) AND time=(?)", \
+                        ( station_ID, self._round_time(curr_time) ) ).rowcount
 
         return num_deleted_rows
 
@@ -142,34 +187,33 @@ class WeatherDB(object):
         """
         with self._lock:
             with self._sql:
-                data_from_db = self._sql.execute( " \
-                                                    SELECT * \
-                                                    FROM WeatherData \
-                                                    WHERE stationID=(?) AND time BETWEEN (?) AND (?)", \
-                                                        ( station_identifier, first_time, last_time ) ).fetchall()
+                base_data_from_db = self._sql.execute( " \
+                    SELECT * \
+                    FROM WeatherData \
+                    WHERE stationID=(?) AND time BETWEEN (?) AND (?) \
+                    ORDER BY time", \
+                    ( station_identifier, first_time, last_time ) ).fetchall()
 
-        data_in_range = [ dict( item ) for item in data_from_db ]
+                wind_data_from_db = self._sql.execute( " \
+                    SELECT * \
+                    FROM WindSensor \
+                    WHERE stationID=(?) AND time BETWEEN (?) AND (?) \
+                    ORDER BY time", \
+                    ( station_identifier, first_time, last_time ) ).fetchall()
+        
+                base_data_in_range = [ dict( item ) for item in base_data_from_db ]
+                wind_data_in_range = [ dict( item ) for item in wind_data_from_db ]
 
-        return data_in_range
+        datasets = []
+        for base, wind in zip( base_data_in_range, wind_data_in_range ):
+            datasets.append( WeatherDataset( base['time'], 0, 0, base["rainGauge"], base["pressure"], base["UV"], wind["direction"], wind["speed"], wind["gusts"], wind["temperature"] ) )
+
+        return datasets
 
 
     def add_station(self, station):
         """
         Adds a new weather station to the database
-        """
-        self._insert_station(station, False)
-
-
-    def replace_station(self,station):
-        """
-        Replaces the metadata of an existing weather station in the database. DO NOT USE this method if a station has been relocated to a new place. Create instead a new station with a new identifier.
-        """
-        self._insert_station(station, True)
-
-
-    def _insert_station(self, station, is_replace):
-        """
-        Inserts weather station metadata to the database
         """
         identifier = station.get_identifier();
         device = station.get_device_info();
@@ -178,23 +222,39 @@ class WeatherDB(object):
 
         with self._lock:
             with self._sql:
-                if is_replace:
-                    commandStr = "REPLACE"
-                else:
-                    commandStr = "INSERT"
-
-                # inserting fails silently if the station already exists
                 try:
-                    self._sql.execute( "%s INTO WeatherStations ( \
-                                        identifier, \
-                                        device, \
-                                        location, \
-                                        latitude, \
-                                        longitude, \
-                                        height) VALUES (?,?,?,?,?,?)" % commandStr, \
-                                            ( identifier, device, location, latitude, longitude, height ) )           
+                    self._sql.execute( " \
+                        INSERT INTO WeatherStations ( \
+                            identifier, \
+                            device, \
+                            location, \
+                            latitude, \
+                            longitude, \
+                            height) VALUES (?,?,?,?,?,?)", \
+                            ( identifier, device, location, latitude, longitude, height ) )                  
                 except sqlite3.Error as e:
                     raise NameError("The station is already existing")
+
+
+    def replace_station(self,station):
+        """
+        Replaces the metadata of an existing weather station in the database. DO NOT USE this method if a station has been relocated to a new place. Create instead a new station with a new identifier.
+        """
+        identifier = station.get_identifier();
+        device = station.get_device_info();
+        location = station.get_location_info();
+        latitude, longitude, height = station.get_geo_info();
+
+        with self._lock:
+            with self._sql:
+                num_updated_rows = self._sql.execute( " \
+                    UPDATE WeatherStations \
+                    SET device=(?), location=(?), latitude=(?), longitude=(?), height=(?) \
+                    WHERE identifier=(?) ", \
+                    ( device, location, latitude, longitude, height, identifier ) ).rowcount           
+           
+                if num_updated_rows == 0:
+                    raise NameError("The station is not existing")
 
 
     def remove_station(self, station_ID):
@@ -203,10 +263,11 @@ class WeatherDB(object):
         """
         with self._lock:
             with self._sql:
-                num_deleted_rows = self._sql.execute( "DELETE \
-                                                            FROM WeatherStations \
-                                                            WHERE identifier=(?)", \
-                                                                ( station_ID, ) ).rowcount
+                num_deleted_rows = self._sql.execute( " \
+                    DELETE \
+                    FROM WeatherStations \
+                    WHERE identifier=(?)", \
+                    ( station_ID, ) ).rowcount
 
         if num_deleted_rows == 1:
             return True
@@ -220,6 +281,12 @@ class WeatherDB(object):
         """
         with self._lock:
             with self._sql:
-                is_existing = self._sql.execute( "SELECT EXISTS (SELECT * FROM WeatherStations WHERE identifier=(?))", ( station_ID, ) ).fetchone()[0]
+                # all weather dataset entries are automatically deleted due to the configured cascade
+                is_existing = self._sql.execute( " \
+                    SELECT EXISTS ( \
+                        SELECT * \
+                        FROM WeatherStations \
+                        WHERE identifier=(?) \
+                    )", ( station_ID, ) ).fetchone()[0]
 
         return is_existing
