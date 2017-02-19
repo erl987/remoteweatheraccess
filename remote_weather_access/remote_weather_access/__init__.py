68b5cc46312388281fb58172adc0a159966dc578
ralf <ralf@localhost>
2017-02-19 21:02:09 +0000

    Fixing the situation when the package version is unknown.
    svn path=/branches/database/; revision=177

--- remote_weather_access/remote_weather_access/__init__.py
+++ remote_weather_access/remote_weather_access/__init__.py
@@ -1,4 +1,8 @@
 import pkg_resources
 
 package_name = "remote-weather-access"
-__version__ = pkg_resources.require(package_name)[0].version
+
+try:
+    __version__ = pkg_resources.require(package_name)[0].version
+except pkg_resources.DistributionNotFound:
+    __version__ = "unknown"
