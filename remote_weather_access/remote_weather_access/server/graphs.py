7550231d809797955ec8271848a5120d01652d0c
ralf <ralf@localhost>
2017-02-19 14:48:40 +0000

    Updating the copyright year to 2017.
    svn path=/branches/database/; revision=171

--- remote_weather_access/remote_weather_access/server/graphs.py
+++ remote_weather_access/remote_weather_access/server/graphs.py
@@ -1,5 +1,5 @@
 # RemoteWeatherAccess - Weather network connecting to remote stations
-# Copyright(C) 2013-2016 Ralf Rettig (info@personalfme.de)
+# Copyright(C) 2013-2017 Ralf Rettig (info@personalfme.de)
 #
 # This program is free software: you can redistribute it and / or modify
 # it under the terms of the GNU General Public License as published by
