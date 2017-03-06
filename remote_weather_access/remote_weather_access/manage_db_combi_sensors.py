f8f7c9dc96a8f58dec566ed155d2b24738d4e14f
ralf <ralf@localhost>
2017-03-06 21:42:29 +0000

    Refactoring the command line interface of the combi sensor manager tool.
    svn path=/branches/database/; revision=203

--- remote_weather_access/remote_weather_access/manage_db_combi_sensors.py
+++ remote_weather_access/remote_weather_access/manage_db_combi_sensors.py
@@ -13,8 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.If not, see <http://www.gnu.org/licenses/>
-
-import sys
+import argparse
 import json
 
 from remote_weather_access.server.sqldatabase import SQLWeatherDB
@@ -25,73 +24,69 @@ def main():
     """
     Program for managing the combi sensors (temperature / humidity) being present in the weather database.
     """
-    if len(sys.argv) != 4 and not (len(sys.argv) == 3 and sys.argv[-1].upper() == "PRINT"):
-        if len(sys.argv) == 2 and sys.argv[-1].lower() == "--version":
-            print("manage-weather-combi-sensors, {} version {}".format(package_name, __version__))
-        else:
-            print("Invalid number of arguments.\n"
-                  "\n"
-                  "Program for managing the combi sensors (temperature / humidity) being present in the weather "
-                  "database.\n"
-                  "WARNING: REMOVING A SENSOR FROM THE DATABASE REMOVES ALL OF ITS DATA, THIS CANNOT BE UNDONE."
-                  "\n"
-                  "Command line arguments:\n"
-                  "manage-weather-combi-sensors [[DB_FILE] [PRINT] | [[TYPE] [JSON-FILE|SENSOR ID]]] | --version\n"
-                  "\n"
-                  "DB_FILE: path of the weather database file\n"
-                  "PRINT: prints all combi sensors present in the database\n"
-                  "TYPE: type of operation (add | remove | replace)\n"
-                  "JSON-FILE: containing all required settings for the added / replaced sensors\n"
-                  "\n"
-                  "Example usages:\n"
-                  "manage-weather-combi-sensors ./weather.db add newSensors.json\n"
-                  "manage-weather-combi-sensors ./weather.db remove OUT1\n"
-                  "manage-weather-combi-sensors ./weather.db replace newSensors.json\n"
-                  "manage-weather-combi-sensors ./weather.db print\n"
-                  "\n"
-                  "Necessary format for the JSON-file:\n"
-                  "{\n"
-                  "     \"IN\": \"Innensensor\",\n",
-                  "     \"OUT\": \"Außensensor 1\"\n"
-                  "}")
-    else:
-        try:
-            db_file_name = sys.argv[1]
-            operation_type = sys.argv[2].upper()
-            sensor_data = None
-            sensor_id = None
+    parser = argparse.ArgumentParser(description="Program for managing the combi sensors (temperature / humidity) "
+                                                 "being present in the weather database. WARNING: REMOVING A SENSOR "
+                                                 "FROM THE DATABASE REMOVES ALL OF ITS DATA, THIS CANNOT BE UNDONE.")
+    parser.add_argument("db_file", metavar="DATABASE", type=str, help="weather database")
+    operation_type = parser.add_mutually_exclusive_group(required=True)
+    operation_type.add_argument("--print", "-p", dest="do_print", action="store_true",
+                                help="print all combi sensors present in the database")
+    operation_type.add_argument("--add", "-a", metavar="SENSOR-DATA-FILE", type=str,
+                                help="add new sensors specified in the JSON-file (containing "
+                                     "{\"SENSOR-ID\": \"description\", ...})")
+    operation_type.add_argument("--remove", "-rm", metavar="SENSOR-ID", type=str,
+                                help="remove the specified sensor")
+    operation_type.add_argument("--replace", "-rp", metavar="SENSOR-DATA-FILE", type=str,
+                                help="replace the sensor data by that specified in the JSON-file (containing "
+                                     "{\"SENSOR-ID\": \"description\", ...})")
+    parser.add_argument("--version", "-v", action="version", version="{} version {}".format(package_name, __version__),
+                        help='show the version information of the program')
 
-            if operation_type == "ADD" or operation_type == "REPLACE":
-                json_metadata_file = sys.argv[3]
-                with open(json_metadata_file) as file:
-                    sensor_data = json.load(file)
-            elif operation_type == "REMOVE":
-                sensor_id = sys.argv[3]
+    args = parser.parse_args()
 
-            # update the database
-            database = SQLWeatherDB(db_file_name)
-            if operation_type == "ADD":
-                for sensor_id in sensor_data:
-                    database.add_combi_sensor(sensor_id, sensor_data[sensor_id])
-            elif operation_type == "REPLACE":
-                for sensor_id in sensor_data:
-                    database.replace_combi_sensor(sensor_id, sensor_data[sensor_id])
-            elif operation_type == "REMOVE":
-                if not database.remove_combi_sensor(sensor_id):
-                    raise SyntaxError("The sensor {} does not exist in the database".format(sensor_id))
-            elif operation_type == "PRINT":
-                sensor_ids, sensor_descriptions = database.get_combi_sensors()
-                print("Combi sensors present in the database:\n")
+    try:
+        db_file_name = args.db_file
+        sensor_data = None
+        sensor_id = None
+        json_metadata_file = None
+
+        if args.add:
+            json_metadata_file = args.add
+        if args.replace:
+            json_metadata_file = args.replace
+        if json_metadata_file:
+            with open(json_metadata_file) as file:
+                sensor_data = json.load(file)
+        if args.remove:
+            sensor_id = args.remove
+
+        # update the database
+        database = SQLWeatherDB(db_file_name)
+        if args.add:
+            for sensor_id in sensor_data:
+                database.add_combi_sensor(sensor_id, sensor_data[sensor_id])
+        elif args.replace:
+            for sensor_id in sensor_data:
+                database.replace_combi_sensor(sensor_id, sensor_data[sensor_id])
+        elif args.remove:
+            if not database.remove_combi_sensor(sensor_id):
+                raise SyntaxError("The sensor {} does not exist in the database".format(sensor_id))
+        elif args.do_print:
+            sensor_ids, sensor_descriptions = database.get_combi_sensors()
+            print("Combi sensors present in the database:\n")
+            if sensor_ids:
                 for sensor_id in sensor_ids:
                     print(sensor_id + ": " + sensor_descriptions[sensor_id])
             else:
-                raise SyntaxError("Invalid command arguments")
+                print("None")
+        else:
+            raise SyntaxError("Invalid command arguments")
 
-            if not operation_type == "PRINT":
-                print("You must restart the weather server now to reload the available stations.")
-            print("\nOperation was successful.")
-        except Exception as e:
-            print("Execution failed: {}. Call the command without options for help.".format(e))
+        if not args.do_print:
+            print("You must restart the weather server now to reload the available stations.")
+        print("\nOperation was successful.")
+    except Exception as e:
+        print("Execution failed: {}. Call the command without options for help.".format(e))
 
 
 if __name__ == "__main__":
