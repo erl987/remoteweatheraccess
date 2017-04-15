9832ae9d2ed051883a19e8ea43b6dd6d8c790948
ralf <ralf@localhost>
2017-04-15 12:10:56 +0000

    Extended the client mock script for providing data for several stations.
    svn path=/branches/database/; revision=211

--- remote_weather_access/tests/mocks/client_mock.py
+++ remote_weather_access/tests/mocks/client_mock.py
@@ -20,14 +20,14 @@ import shutil
 import time
 from zipfile import ZipFile
 
-from remote_weather_access.common import utilities
+from remote_weather_access.server.exceptions import NotExistingError
 from remote_weather_access.common.fileformats import PCWetterstationFormatFile
 from remote_weather_access.common.datastructures import WeatherStationDataset, BaseStationSensorData, WindSensorData, \
     CombiSensorData, RainSensorData
 from remote_weather_access.server.sqldatabase import SQLWeatherDB
 
-station_id = "NBG"
-receiver_dir_path = "/var/lib/remote-weather-access/receiver" + os.sep + station_id
+station_ids = ["NBG", "ERL", "ECK"]
+receiver_base_dirpath = "/var/lib/remote-weather-access/receiver"
 temp_dir_path = "/tmp"
 db_file_name = "/var/lib/remote-weather-access/weather.db"
 archive_file_type = "zip"
@@ -50,52 +50,62 @@ def main():
     install in "/usr/lib/python3/dist-packages/remote_weather_access"
     run by installing the systemd-service "clientmock"
     """
-    # provide a first time close to real time, rounded to full 10-minutes
-    curr_time = datetime.datetime.now()
-    next_time = datetime.datetime.fromtimestamp(utilities.ceil_to_n(curr_time.timestamp(), 10 * 60))
+    # determine the first time based on the earliest last database entry
+    database = SQLWeatherDB(db_file_name)
+    next_times = []
+    for station_id in station_ids:
+        try:
+            next_times.append(database.get_most_recent_time_with_data(station_id) + datetime.timedelta(minutes=10))
+        except NotExistingError:
+            pass
+    next_time = min(next_times)
 
     # load the station metadata from the database
-    database = SQLWeatherDB(db_file_name)
-    station_metadata = database.get_station_metadata(station_id)
+    station_metadata = dict()
+    for station_id in station_ids:
+        station_metadata[station_id] = database.get_station_metadata(station_id)
+
+    # load the combi sensor descriptions from the database
+    combi_sensor_ids, combi_sensor_descriptions = database.get_combi_sensors()
 
     while True:
-        # load the combi sensor descriptions from the database
-        combi_sensor_ids, combi_sensor_descriptions = database.get_combi_sensors()
+        for station_id in station_ids:
+            # create data
+            dataset = WeatherStationDataset(next_time)
+            dataset.add_sensor(BaseStationSensorData(1032.4, 8.5))
+            dataset.add_sensor(WindSensorData(12.4, 43.9, 180.0, 15.2))
+            for combi_id in combi_sensor_ids:
+                dataset.add_sensor(CombiSensorData(combi_id, 34.9, 89.7, combi_sensor_descriptions[combi_id]))
+            dataset.add_sensor(RainSensorData(12.5, next_time - datetime.timedelta(minutes=10)))
+            data = [dataset]
 
-        # create random data
-        dataset = WeatherStationDataset(next_time)
-        dataset.add_sensor(BaseStationSensorData(1032.4, 8.5))
-        dataset.add_sensor(WindSensorData(12.4, 43.9, 180.0, 15.2))
-        for combi_id in combi_sensor_ids:
-            dataset.add_sensor(CombiSensorData(combi_id, 34.9, 89.7, combi_sensor_descriptions[combi_id]))
-        dataset.add_sensor(RainSensorData(12.5, next_time - datetime.timedelta(minutes=10)))
-        data = [dataset]
+            archive_file_name = None
+            curr_temp_dir_path = None
+            try:
+                # create a new temporary directory
+                curr_time_string = str(datetime.datetime.utcnow().timestamp())
+                curr_time_string = curr_time_string.replace(".", "_")
+                curr_temp_dir_path = temp_dir_path + os.sep + curr_time_string
+                os.mkdir(curr_temp_dir_path)
 
-        archive_file_name = None
-        curr_temp_dir_path = None
-        try:
-            # create a new temporary directory
-            curr_time_string = str(datetime.datetime.utcnow().timestamp())
-            curr_time_string = curr_time_string.replace(".", "_")
-            curr_temp_dir_path = temp_dir_path + os.sep + curr_time_string
-            os.mkdir(curr_temp_dir_path)
+                weather_data_file = PCWetterstationFormatFile(combi_sensor_ids, combi_sensor_descriptions)
+                weather_data_file.write(curr_temp_dir_path, data, station_metadata[station_id])
 
-            weather_data_file = PCWetterstationFormatFile(combi_sensor_ids, combi_sensor_descriptions)
-            weather_data_file.write(curr_temp_dir_path, data, station_metadata)
+                # copy the data file to the weather server receiver directory for consumption
+                archive_file_name = temp_dir_path + os.sep + curr_time_string + "_" + station_id + "." +\
+                                    archive_file_type
+                with ZipFile(archive_file_name, mode='w') as archive:
+                    for file in os.listdir(curr_temp_dir_path):
+                        if file.upper().endswith(data_file_type.upper()):
+                            archive.write(os.path.join(curr_temp_dir_path, file), file)
 
-            # copy the data file to the weather server receiver directory for consumption
-            archive_file_name = temp_dir_path + os.sep + curr_time_string + "_" + station_id + "." + archive_file_type
-            with ZipFile(archive_file_name, mode='w') as archive:
-                for file in os.listdir(curr_temp_dir_path):
-                    if file.upper().endswith(data_file_type.upper()):
-                        archive.write(os.path.join(curr_temp_dir_path, file), file)
-
-            shutil.copy(archive_file_name, receiver_dir_path)
-        finally:
-            if archive_file_name and os.path.exists(archive_file_name):
-                os.remove(archive_file_name)
-            if curr_temp_dir_path:
-                shutil.rmtree(curr_temp_dir_path, ignore_errors=True)
+                receiver_dir_path = receiver_base_dirpath + os.sep + station_id
+                shutil.copy(archive_file_name, receiver_dir_path)
+            finally:
+                if archive_file_name and os.path.exists(archive_file_name):
+                    os.remove(archive_file_name)
+                if curr_temp_dir_path:
+                    shutil.rmtree(curr_temp_dir_path, ignore_errors=True)
 
         next_time += datetime.timedelta(minutes=10)
         time.sleep(random_delay_time())
