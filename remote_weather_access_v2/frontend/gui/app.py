d25d27575f298f962130cd593a26b086c7772146
Ralf Rettig <meteoradix@gmx.net>
2020-04-04 13:19:01 +0200

    Changed the order of colors for the plot quantities.

--- remote_weather_access_v2/frontend/gui/app.py
+++ remote_weather_access_v2/frontend/gui/app.py
@@ -22,16 +22,15 @@ app = dash.Dash(__name__)
 
 config_plots = {"locale": "de"}
 
-# default plot.ly colors
 color_list = [
     "#1f77b4",  # muted blue
-    "#ff7f0e",  # safety orange
     "#2ca02c",  # cooked asparagus green
     "#d62728",  # brick red
+    "#7f7f7f",  # middle gray
     "#9467bd",  # muted purple
+    "#ff7f0e",  # safety orange
     "#8c564b",  # chestnut brown
     "#e377c2",  # raspberry yogurt pink
-    "#7f7f7f",  # middle gray
     "#bcbd22",  # curry yellow-green
     "#17becf"]  # blue-teal
 
