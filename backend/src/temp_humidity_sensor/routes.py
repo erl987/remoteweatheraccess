ef42baf4edf1b5a7889018017da5b778ddfc8f11
Ralf Rettig <meteoradix@gmx.net>
2020-09-12 17:16:01 +0200

    Added copyright information to all relevant files.

--- backend/src/temp_humidity_sensor/routes.py
+++ backend/src/temp_humidity_sensor/routes.py
@@ -1,3 +1,19 @@
+#  Remote Weather Access - Client/server solution for distributed weather networks
+#   Copyright (C) 2013-2020 Ralf Rettig (info@personalfme.de)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from http import HTTPStatus
 
 from flask import Blueprint, jsonify, current_app
